fda4183b8b21f0e16ef0a76e05f1dfd441301711
Aryan <aryanarikhanna111@gmail.com>
2023-08-14 14:17:31 +0530

    test

---
+++ test.js
@@ -0,0 +1,66 @@
+function copyy(form) 
+{  
+  /* main + 1st*/
+form.qq.value = form.q.value;
+form.customercopy.value = form.customer.value;  
+form.dc.value = form.d.value;   
+form.bc.value = form.b.value;
+form.w.value = form.wc.value;
+form.qtc.value = form.qt.value;
+form.sgstc.value = form.sgst.value;
+form.totalc.value = form.total.value;
+form.tc.value = form.t.value
+form.gstc.value = form.gst.value;
+/* 2nd*/
+form.qq2.value = form.q2.value;
+form.w2.value = form.wc2.value;
+form.qtc2.value = form.qt2.value;
+form.sgstc2.value = form.sgst2.value;
+form.gstc2.value = form.gst2.value;
+form.tc2.value = form.t2.value;
+/* 3rd */
+form.qq3.value = form.q3.value;
+form.w3.value = form.wc3.value;
+form.qtc3.value = form.qt3.value;
+form.sgstc3.value = form.sgst3.value;
+form.gstc3.value = form.gst3.value;
+form.tc3.value = form.t3.value;
+/*4th*/
+form.qq4.value = form.q4.value;
+form.w4.value = form.wc4.value;
+form.qtc4.value = form.qt4.value;
+form.sgstc4.value = form.sgst4.value;
+form.gstc4.value = form.gst4.value;
+form.tc4.value = form.t4.value;
+/* 5th*/
+form.qq5.value = form.q5.value;
+form.w5.value = form.wc5.value;
+form.qtc5.value = form.qt5.value;
+form.sgstc5.value = form.sgst5.value;
+form.gstc5.value = form.gst5.value;
+form.tc5.value = form.t5.value;
+/*6th*/
+form.qq6.value = form.q6.value;
+form.w6.value = form.wc6.value;
+form.qtc6.value = form.qt6.value;
+form.sgstc6.value = form.sgst6.value;
+form.gstc6.value = form.gst6.value;
+form.tc6.value = form.t6.value;
+/* 7th*/
+form.qq7.value = form.q7.value;
+form.w7.value = form.wc7.value;
+form.qtc7.value = form.qt7.value;
+form.sgstc7.value = form.sgst7.value;
+form.gstc7.value = form.gst7.value;
+form.tc7.value = form.t7.value;
+
+/* 8th*/
+form.qq8.value = form.q8.value;
+form.w8.value = form.wc8.value;
+form.qtc8.value = form.qt8.value;
+form.sgstc8.value = form.sgst8.value;
+form.gstc8.value = form.gst8.value;
+form.tc8.value = form.t8.value;
+
+}
+  
